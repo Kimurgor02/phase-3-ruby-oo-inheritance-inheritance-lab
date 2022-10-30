37a2cb1f0b9a332e12b611855306baa27f63f2db
Allan <allan.kimurgor@student.moringaschool.com>
2022-10-30 12:39:13 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,27 @@
-class Student 
+#require_relative './user'
+#class Student < User
+ #   def initialize
+  #      @knowledge = []
+   # end
+    #def learn (know)
+     #   @knowledge << know
+    #end
+    #def knowledge
+     #   @knowledge
+    #end
+#end
 
+require_relative './user'
+
+class Student < User
+ def initialize 
+    @knowledge = []
+ end
+ def learn (know)
+    @knowledge << know
+
+ end
+ def knowledge
+    @knowledge
+ end
 end
